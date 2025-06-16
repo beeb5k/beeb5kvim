0aac794b7343cfa7a69a953989987a9dc4a163a4
beeb5k <beeb5k6@gmail.com>
2025-06-17 00:21:02 +0530

    add grapple config

--- lua/config/remaps.lua
+++ lua/config/remaps.lua
@@ -11,7 +11,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 
 vim.keymap.set("n", "q:", "<nop>") -- remove that annoying cmd history buffer
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
