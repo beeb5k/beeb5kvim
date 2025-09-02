7a93e5f39bbab5a64f11fa9ad37d4804d7f5079a
beeb5k <beeb5k6@gmail.com>
2025-09-02 11:33:13 +0530

    More neovim pkgs

--- package.nix
+++ package.nix
@@ -1,49 +1,65 @@
 inputs:
 let
   inherit (inputs.nixCats) utils;
-in
-{
-  Neovim =
-    { pkgs, name, ... }:
+  baseSettings =
+    { pkgs, name, ... }@misc:
     {
-      settings = {
-        suffix-path = true;
-        suffix-LD = true;
-        hosts.node.enable = false;
-        hosts.ruby.enable = false;
-        hosts.perl.enable = false;
-        hosts.python3.enable = false;
-        wrapRc = true;
-        useBinaryWrapper = true;
-        aliases = [
-          "vim"
-          "vi"
-          "nvim"
-        ];
-        # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
-      };
-      categories = {
-        blink = true;
-        ui = true;
-        git = true;
-        theme = true;
-        rust = true;
-        nix = true;
-        lua = true;
-        javascript = true;
-        go = false;
-        java = false;
-        clang = true;
-        python = false;
-        extras = true;
-        general = true;
-        markdown = false;
-        lspDebugMode = false;
-      };
-      extra = {
-        nixdExtras = {
-          nixpkgs = ''import ${pkgs.path} {}'';
-        };
+      suffix-path = true;
+      suffix-LD = true;
+      hosts.node.enable = false;
+      hosts.ruby.enable = false;
+      hosts.perl.enable = false;
+      hosts.python3.enable = false;
+      wrapRc = true;
+      useBinaryWrapper = true;
+      # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+    };
+  baseCats =
+    { pkgs, ... }@misc:
+    {
+      blink = true;
+      ui = true;
+      git = true;
+      theme = true;
+      rust = true;
+      nix = true;
+      lua = true;
+      javascript = true;
+      go = false;
+      java = false;
+      clang = true;
+      python = false;
+      extras = true;
+      general = true;
+      markdown = false;
+      lspDebugMode = false;
+    };
+  baseExtra =
+    { pkgs, ... }@misc:
+    {
+      nixdExtras = {
+        nixpkgs = ''import ${pkgs.path} {}'';
       };
     };
+in
+{
+  Neovim = args: {
+    settings = baseSettings args // {
+      aliases = [
+        "vim"
+        "vi"
+        "nvim"
+      ];
+      # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+    };
+    categories = baseCats args;
+    extra = baseExtra args;
+  };
+  mvim = args: {
+    settings = baseSettings args // {
+      suffix-path = false;
+      suffix-LD = false;
+      aliases = [ ];
+    };
+  };
 }
